3147c76802b8dc0da3786df802b5596ffc5f9072
alejandragutierrezglez <alejandragutierrezglez2023@gmail.com>
2023-12-26 12:29:24 -0600

    commit ML Nelly

---
+++ ML/RecetaIngrediente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class RecetaIngrediente
+    {
+        public ML.Receta Receta { get; set; }   
+
+        public ML.Ingrediente Ingrediente { get; set; }
+
+        public bool Correct { get; set; }   
+
+        public Object Object { get; set; }  
+
+        public List<Object> Objects { get; set; }   
+
+    }
+}
---
+++ ML/RestauranteReceta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class RestauranteReceta
+    {
+        public ML.Restaurante Restaurante { get; set; }
+
+        public ML.Receta Receta { get; set; }
+
+        public bool Correct { get; set; }   
+
+        public Object Object { get; set; }  
+
+        public List<Object> Objects { get; set; }   
+
+    }
+}
