5751158d8edb42d8d05daa2036c916c5ecb8390b
MSandoval00 <msandovalg00@gmail.com>
2024-01-02 12:58:17 -0600

    CORS

--- SL/Controllers/RestauranteController.cs
+++ SL/Controllers/RestauranteController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace SL.Controllers
 {
     [RoutePrefix("api/Restaurante")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class RestauranteController : ApiController
     {
         [Route("")]
